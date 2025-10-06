ba639744fc399c029139e4bf227d7fc4db09e51d
Liza <kuznecovaElizaveta2006@yandex.ru>
2025-10-06 23:06:26 +0300

    feat: connect frontend with Django backend

--- backend/library_project/settings.py
+++ backend/library_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library_app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'library_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'library_app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
