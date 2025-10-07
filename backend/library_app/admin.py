a2c168e0aa53e12d47d64b8ef71165c8088ed798
Liza <kuznecovaElizaveta2006@yandex.ru>
2025-10-08 02:15:02 +0300

    create Book/Author models with admin UI

--- backend/library_app/admin.py
+++ backend/library_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmib(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'year', 'available')
+    list_filter = ('available', 'year', 'genre')
+    search_fields = ('title', 'author_name')
--- backend/library_app/models.py
+++ backend/library_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя автора")
+    bio = models.TextField(blank=True, verbose_name="Биография")
+
+    def __str__(self):
+        return self.name
+        
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название")
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        verbose_name="Автор"
+    )
+    genre = models.CharField(max_length=100, verbose_name="Жанр")
+    year = models.IntegerField(verbose_name="Год издания")
+    isbn = models.CharField(max_length=17, unique=True, verbose_name="ISBN")
+    description = models.TextField(verbose_name="Описание")
+    available = models.BooleanField(default=True, verbose_name="Доступна")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
