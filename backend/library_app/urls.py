84a739301aa54f050d50d2f516b648ee71d45c2d
Liza <kuznecovaElizaveta2006@yandex.ru>
2025-10-08 09:03:03 +0300

    view database

--- backend/library_app/urls.py
+++ backend/library_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
--- backend/library_app/views.py
+++ backend/library_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Book
+
 
 def index(request):
-    return render(request, 'index.html')
+    books = Book.objects.filter(available=True).order_by('id')[:6]
+    return render(request, 'index.html', {'books' : books})
+
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
