203f8d98b6027d95a14b6ff7e6e6a441999aa872
Liza <kuznecovaElizaveta2006@yandex.ru>
2025-10-10 01:01:07 +0300

    configuring book search

--- backend/library_app/urls.py
+++ backend/library_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('books/', views.books_list, name='books_list'),
 ]
--- backend/library_app/views.py
+++ backend/library_app/views.py
@@ -10,3 +10,15 @@ def index(request):
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'book_detail.html', {'book': book})
+
+
+def books_list(request):
+    query = request.GET.get('q', '')
+    if query:
+        books = Book.objects.filter(
+            available = True,
+            title__icontains=query
+        ).order_by('title')
+    else:
+        books = Book.objects.filter(available=True).order_by('title')
+    return render(request, 'books.html', {'books': books, 'query': query})
